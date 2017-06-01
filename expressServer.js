23089b9d3b4006115c7e4cfd8d45063e87fc4be3
Kurtis Houser <houserkurtis@gmail.com>
2017-06-01 08:30:45 -0700

    Add POST /pets route and body-parser

--- expressServer.js
+++ expressServer.js
@@ -6,6 +6,14 @@ const dbFile = path.join(__dirname, 'pets.json');
 
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+
+const bodyParser = require('body-parser');
+
+app.disable('x-powered-by');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/pets', (req, res) => {
   fs.readFile(dbFile, (err, data) => {
@@ -19,6 +27,44 @@ app.get('/pets', (req, res) => {
     res.json(pets);
   });
 });
+
+
+app.post('/pets', (req, res) => {
+  let age = Number.parseInt(req.body.age);
+  let kind = req.body.kind;
+  let name = req.body.name;
+
+  if (Number.isNaN(age) || !kind || !name) {
+    res.set('Content-Type', 'text/plain');
+    res.status(400).send('Bad Request');
+  } else {
+    fs.readFile(dbFile, (err, data) => {
+
+      if (err) {
+        console.error(err.stack);
+        res.sendStatus(500);
+      }
+
+      let pets = JSON.parse(data);
+      let newPet = {
+        age: age,
+        kind: kind,
+        name: name
+      }
+      pets.push(newPet);
+
+      fs.writeFile(dbFile, JSON.stringify(pets), (err) => {
+        if (err) {
+          console.error(err.stack);
+          res.sendStatus(500);
+        }
+        // res.set('Content-Type', 'application/json');
+        res.json(newPet);
+      });
+    });
+  } // END: if - else
+});
+
 
 app.get('/pets/:index', (req, res) => {
   fs.readFile(dbFile, (err, data) => {
@@ -39,8 +85,14 @@ app.get('/pets/:index', (req, res) => {
   });
 });
 
+
+app.get('/*', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.status(404).send('Not Found');
+})
+
 app.listen(8000, () => {
-  console.log('listening on port 8000');
+  console.log('listening on port', port);
 })
 
 module.exports = app;
