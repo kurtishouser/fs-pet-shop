2d96762aeb435fe4282352bfc8b6fcbd2ea3f92b
Kurtis Houser <houserkurtis@gmail.com>
2017-06-05 09:00:24 -0700

    Add DELETE /pets/:index route

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -12,7 +12,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 app.disable('x-powered-by');
-app.use(morgan('short'));
+// app.use(morgan('short'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -81,6 +81,65 @@ app.get('/pets/:index', (req, res) => {
     }
     res.json(pets[index]);
   });
+});
+
+
+app.patch('/pets/:index', (req, res) => {
+  let index = Number.parseInt(req.params.index);
+
+  if (Number.isNaN(index)) { // NaN? stop here, no need to check anything else
+    return res.sendStatus(400);
+  }
+
+  let age = Number.parseInt(req.body.age);
+  let kind = req.body.kind;
+  let name = req.body.name;
+  let petObj = {};
+
+  if (age) {
+    // console.log('age provided');
+    if (Number.isNaN(age)) {
+      console.log('invalid age');
+      return res.sendStatus(400);
+    }
+    petObj.age = age;
+  }
+  if (kind) {
+    petObj.kind = kind;
+  }
+  if (name) {
+    petObj.name = name;
+  }
+
+
+  if (petObj.age || petObj.kind || petObj.name) {
+
+    fs.readFile(dbFile, (err, data) => {
+      if (err) {
+        console.err(err.stack);
+        res.sendStatus(500);
+      }
+
+      let pets = JSON.parse(data);
+
+      if (index < 0 || index >= pets.length) {
+        return res.sendStatus(404);
+      }
+
+      let updatePet = Object.assign({}, pets[index], petObj);
+      pets[index] = updatePet;
+
+      fs.writeFile(dbFile, JSON.stringify(pets), (err) => {
+        if (err) {
+          console.error(err.stack);
+          return res.sendStatus(500);
+        }
+        res.json(pets[index]);
+      });
+    });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 
